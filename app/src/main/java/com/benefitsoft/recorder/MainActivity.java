18ea4a6219fdc16a283580abb958ff425afb73ca
kimyuhyun <escnet@naver.com>
2020-09-14 18:47:41 +0900

    2

--- app/src/main/java/com/benefitsoft/recorder/MainActivity.java
+++ app/src/main/java/com/benefitsoft/recorder/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        //1
+        //2
 
         //툴바 삭제
         ActionBar actionBar = getSupportActionBar();
